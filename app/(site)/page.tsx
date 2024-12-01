d5c005738d2d7299a8e4f0f8ec365c490faf0b50
Bulgan-Erdene Altankhuyag <bulganerdene021@gmail.com>
2024-12-01 18:06:24 +0800

    Sidebar эхний өөрчлөлтүүд хийлээ

---
+++ app/(site)/page.tsx
@@ -0,0 +1,18 @@
+import ListItem from "@/components/ListItem";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
+    </div>
+  );
+}
